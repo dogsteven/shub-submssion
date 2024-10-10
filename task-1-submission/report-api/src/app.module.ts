6189b17da858c5108c1db714a8a8c757fcb2e9da
Khoa Huynh Bach <caigicungduoc23@gmail.com>
2024-10-10 19:38:29 +0700

    remove TimeTransform from providers list

--- task-1-submission/report-api/src/app.module.ts
+++ task-1-submission/report-api/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController, TimeTransform } from './app.controller';
+import { AppController } from './app.controller';
 import { ReportService } from './app.report.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [ReportService, TimeTransform],
+  providers: [ReportService],
 })
 export class AppModule {}
